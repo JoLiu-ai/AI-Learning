2acea5cf0fc9548d5217bec5bac13754416d44b0
Jo <hinswhale@yeah.net>
2024-10-26 21:29:11 +0800

    Create LangChain learning-lesson3.py

---
+++ agents/practice/LangChain learning-lesson3.py
@@ -0,0 +1,32 @@
+"""LangChain for LLM Application Development"""
+
+
+MULTI_PROMPT_ROUTER_TEMPLATE = """Given a raw text input to a \
+language model select the model prompt best suited for the input. \
+You will be given the names of the available prompts and a \
+description of what the prompt is best suited for. \
+You may also revise the original input if you think that revising\
+it will ultimately lead to a better response from the language model.
+
+<< FORMATTING >>
+Return a markdown code snippet with a JSON object formatted to look like:
+```json
+{{{{
+    "destination": string \  "DEFAULT" or name of the prompt to use in {destinations}
+    "next_inputs": string \ a potentially modified version of the original input
+}}}}
+```
+
+REMEMBER: 
+"destination" -if the input is not well-suited for any of the candidate prompts, set it as "DEFAULT".
+
+REMEMBER: "next_inputs" can just be the original input \
+if you don't think any modifications are needed.
+
+<< CANDIDATE PROMPTS >>
+{destinations}
+
+<< INPUT >>
+{{input}}
+
+<< OUTPUT (remember to include the ```json)>>"""
